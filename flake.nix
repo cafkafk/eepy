5968c892c0e6de720d6176a906634f34f41bef20
Christina Sørensen <ces@fem.gg>
2025-07-05 22:41:06 +0200

    feat(nix): add NixOS vm test

--- flake.nix
+++ flake.nix
@@ -9,19 +9,37 @@
   outputs = { self, nixpkgs, flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit self inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      perSystem = { pkgs, ... }: {
-        packages.default = pkgs.buildGoModule {
-          pname = "eepy";
-          version = "0.1.0";
-          src = ./.;
-          vendorHash = "sha256-mknBOUz8Xpqz9Jm355uVbky5jmEsRjmk84WrCQ8txyk=";
-        };
+      perSystem = { pkgs, ... }:
+        let
+          eepy-pkg = pkgs.buildGoModule {
+            pname = "eepy";
+            version = "0.1.0";
+            src = ./.;
+            vendorHash = "sha256-mknBOUz8Xpqz9Jm355uVbky5jmEsRjmk84WrCQ8txyk=";
+          };
+        in
+        {
+          packages.default = eepy-pkg;
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.go
-          ];
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.go
+            ];
+          };
+
+          checks.default = pkgs.nixosTest {
+            name = "eepy-test";
+            nodes.machine = {
+              environment.systemPackages = [ eepy-pkg ];
+            };
+            testScript = ''
+              machine.wait_for_unit("multi-user.target")
+              output = machine.succeed("eepy 08:00")
+              assert "Your sleep calibration plan:" in output
+              assert "Wake up at 08:00" in output
+              assert "Go to bed at 23:00" in output
+            '';
+          };
         };
-      };
     };
 }
